abed084b6643f6da44b8f8055d9d751f088cd37f
Ben Mills <fuzzyllama@gmail.com>
2021-11-03 01:06:22 -0700

    Finishes Feature 2 and 3

--- server.js
+++ server.js
@@ -11,10 +11,13 @@ const app = express();
 app.use(cors());
 
 const handleWeatherRequest = (request, response) => {
-  console.log('Got a request.');
-  console.log(request.query);
-  const data = weatherData.find(element => element.lon.toString() === request.query.lon && element.lat.toString() === request.query.lat && element.city_name.toLowerCase() === request.query.city_name);
-  console.log(data);
+  const data = weatherData.find(element => {
+    return (
+      element.lon.toString().slice(0, element.lon.toString().indexOf('.') + 2) === request.query.lon &&
+      element.lat.toString().slice(0, element.lat.toString().indexOf('.') + 2) === request.query.lat &&
+      element.city_name.toLowerCase().split(',')[0] === request.query.city_name
+    )
+  });
   if (data) {
     const forcastData = data.data.map(element => {
       let date = element.valid_date;
@@ -23,7 +26,9 @@ const handleWeatherRequest = (request, response) => {
     });
     response.status(200).send(forcastData);
   } else {
-    response.status(404).send('Sorry, I got nothing.');
+    throw new Error('Broken')
+    //response.status(500).send('Pretty bad stuff.');
+    //response.status(404).send('Sorry, I got nothing.');
   }
 }
 
