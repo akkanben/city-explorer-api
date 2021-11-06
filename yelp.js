564e6a24782d09166722afcdd5a68fe005aa432d
Ben Mills <fuzzyllama@gmail.com>
2021-11-06 14:52:42 -0700

    Removes console logs

--- yelp.js
+++ yelp.js
@@ -7,7 +7,6 @@ const cache = require('./cache');
 const handleYelpRequest = async (request, response) => {
   const {city} = request.query;
   const url = `https://api.yelp.com/v3/businesses/search?categories=restaurants&limit=20&location=${city}`;
-  console.log(url)
   const key = `yelp#${city}`;
   if (cache[key] && (Date.now() - cache[key].timestamp < 3600000)) { // New data if data older than 1 hour
     response.status(200).send(cache[key].yelpData);
@@ -17,7 +16,6 @@ const handleYelpRequest = async (request, response) => {
     try {
       const apiResults = await axios.get(url, {headers: {'Authorization': `Bearer ${process.env.YELP_API_KEY}`}});
       cache[key].yelpData = apiResults.data.businesses.map(element => new Restaurant(element, cache[key].timestamp));
-      console.log(cache[key].yelpData);
       response.status(200).send(cache[key].yelpData);
     } catch (event) {
       response.status(404).send('Nothing good');
